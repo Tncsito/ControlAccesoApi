066a402e537f56f80daf283f72f09a7d22a9839c
tncsito <eduardosantoyo5322100257@correcaminos.uts.edu.mx>
2025-04-02 12:20:47 -0700

    #

---
+++ ControlAccesoApi/Modelos/Circuitos.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ControlAccesoApi.Modelos
+{
+    public class Circuitos
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("Permiso_id")]
+        public string? PermisoId { get; set; }
+        [BsonElement("Estado")]
+        public bool? Estado { get; set; }
+        [BsonElement("Fecha")]
+        public DateTime? Fecha { get; set; }
+    }
+    public class CircuitosDto
+    {
+        public string? PermisoId { get; set; }
+        public DateTime? Fecha { get; set; }
+        public bool? Estado { get; set; }
+
+    }
+}
